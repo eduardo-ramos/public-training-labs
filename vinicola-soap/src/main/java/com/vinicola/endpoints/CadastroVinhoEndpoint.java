317b3480d4b23754a6d0d151f7091b3a141a9868
Eduardo Ramos <161091286+eduardo-ramos@users.noreply.github.com>
2025-05-30 01:13:23 +0000

    first running version

---
+++ vinicola-soap/src/main/java/com/vinicola/endpoints/CadastroVinhoEndpoint.java
@@ -0,0 +1,22 @@
+package com.vinicola.endpoints;
+
+import org.springframework.ws.server.endpoint.annotation.Endpoint;
+import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
+import org.springframework.ws.server.endpoint.annotation.RequestPayload;
+import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
+
+import br.com.vinicola.vinhos.CadastrarVinhoRequest;
+import br.com.vinicola.vinhos.CadastrarVinhoResponse;
+
+@Endpoint
+public class CadastroVinhoEndpoint {
+    private static final String NAMESPACE_URI = "http://vinicola.com.br/vinhos";
+
+    @PayloadRoot(namespace = NAMESPACE_URI, localPart = "CadastrarVinhoRequest")
+    @ResponsePayload
+    public CadastrarVinhoResponse cadastrarVinho(@RequestPayload CadastrarVinhoRequest request) {
+        CadastrarVinhoResponse response = new CadastrarVinhoResponse();
+        response.setStatus("Vinho cadastrado com sucesso: " + request.getNome());
+        return response;
+    }
+}
