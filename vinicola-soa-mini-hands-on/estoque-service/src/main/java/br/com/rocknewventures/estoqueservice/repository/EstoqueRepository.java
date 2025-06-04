1bbef87dd0ccc74a0933df2dfdaae15ab3a73689
Eduardo Ramos <161091286+eduardo-ramos@users.noreply.github.com>
2025-06-04 01:45:21 +0000

    soa mini hands on

--- vinicola-soa-mini-hands-on/estoque-service/src/main/java/br/com/rocknewventures/estoqueservice/repository/EstoqueRepository.java
+++ vinicola-soa-mini-hands-on/estoque-service/src/main/java/br/com/rocknewventures/estoqueservice/repository/EstoqueRepository.java
@@ -1,7 +1,6 @@
 package br.com.rocknewventures.estoqueservice.repository;
 
 import java.math.BigInteger;
-import java.util.List;
 import java.util.UUID;
 
 import org.springframework.stereotype.Repository;
